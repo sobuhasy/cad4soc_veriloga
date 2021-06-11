1e3474289f85a8fd83795ed93d35ec796b8fe902
Dominic Korner <Dominic.Korner@ies.tu-darmstadt.de>
2021-06-11 11:02:55 +0200

    Update start.sh to Cadence 2021

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 # and then starts Virtuoso. Running in a subshell suppresses
 # messages like [PID] or Done. And all output is redirected
 # to /dev/null.
-(module load cadence/rc; virtuoso >& /dev/null &)
+(module load 2021/cadence/Cadence2021_eda; virtuoso >& /dev/null &)
