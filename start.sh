380f93a0d43a38ec69e460b3cb63d3f14740e57f
Ferdinand Keil <Ferdinand.Keil@ies.tu-darmstadt.de>
2022-07-12 18:59:06 +0200

    added passwordless login to start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,24 @@
 #!/bin/tcsh -f
 source $home/.cshrc
 
+set SIMSERVER = "amnesix.ies.e-technik.tu-darmstadt.de"
+
 echo "CADENCE VIRTUOSO LAUNCHER"
 echo ""
 
-ssh -X amnesix 'tcsh -s' < /home/$USER/cad4soc_veriloga/virtuoso.sh
+# so if there's no SSH key we'll deal with that...
+if (! -e ~/.ssh/id_rsa) then
+    # first we have to fix the permissions :(
+    chmod go-w "/home/$user"
+    if (! -d ~/.ssh ) then
+        mkdir ~/.ssh
+    endif
+    chmod 700 ~/.ssh
+    # generate SSH key
+    ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
+    # add it so it can be used for authorization
+    cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys
 
+# connect to the simulation server and add its host fingerprint
+# without prompting the user
+ssh -o StrictHostKeyChecking=accept-new -X -l "$user" "$SIMSERVER" 'tcsh -s' < /home/$USER/cad4soc_veriloga/virtuoso.sh
