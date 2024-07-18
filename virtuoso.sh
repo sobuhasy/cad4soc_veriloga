c74b2ff5cacc982817df555baa2fb519810b7393
PraktikumGruppe CAD4Soc05 <c4s05@einsilbix.ies.e-technik.tu-darmstadt.de>
2024-07-18 10:20:36 +0200

    first commit

--- virtuoso.sh
+++ virtuoso.sh
@@ -4,5 +4,5 @@
 # and then starts Virtuoso. Running in a subshell suppresses
 # messages like [PID] or Done. And all output is redirected
 # to /dev/null.
-cd /home/$USER/cad4soc_veriloga/
+# cd /home/$USER/cad4soc_veriloga/
 (module load 2021/cadence/Cadence2021_eda; virtuoso >& /dev/null &)
